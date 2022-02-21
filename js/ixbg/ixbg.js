301066b5b55f972991b83d8a87e54b358398d7c1
chris <dwatow@gmail.com>
2022-02-21 22:30:56 +0800

    Site updated: 2022-02-21 22:30:55

--- js/ixbg/ixbg.js
+++ js/ixbg/ixbg.js
@@ -1,27 +1,26 @@
 (function () {
-  var resizeWidth;
-  var resizeHeight;
-
   //initial canvas
-  const canvas = document.querySelector('#ixbg');
-  const sidebar = document.querySelector('.SideBar');
-  canvas.width = sidebar.offsetWidth
-  canvas.height = sidebar.offsetHeight
+  const canvas = document.querySelector("#ixbg");
+  const sidebar = document.querySelector(".SideBar");
+  canvas.width = sidebar.offsetWidth;
+  canvas.height = sidebar.offsetHeight;
 
   //init window event
-  const requestAnimationFrame = window.requestAnimationFrame ||
+  const requestAnimationFrame =
+    window.requestAnimationFrame ||
     window.webkitRequestAnimationFrame ||
     window.mozRequestAnimationFrame ||
     window.oRequestAnimationFrame ||
     window.msRequestAnimationFrame ||
     function (i) {
-      window.setTimeout(i, 1000 / 45)
-    }
+      window.setTimeout(i, 1000 / 45);
+    };
 
-
-  function resize () {
-    resizeWidth = canvas.width;// = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    resizeHeight = canvas.height;// = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+  var resizeWidth;
+  var resizeHeight;
+  function resize() {
+    resizeWidth = canvas.width; // = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    resizeHeight = canvas.height; // = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
   }
   window.onresize = resize;
   resize();
@@ -29,12 +28,12 @@
   const mousePosition = {
     x: null,
     y: null,
-    max: 100
+    max: 100,
   };
   window.onmousemove = function (e) {
-    e = e || window.event,
-      mousePosition.x = e.pageX,
-      mousePosition.y = e.pageY
+    (e = e || window.event),
+      (mousePosition.x = e.pageX),
+      (mousePosition.y = e.pageY);
   };
 
   window.onmouseout = function () {
@@ -43,53 +42,65 @@
   };
 
   //initial position in canvas
-  const totalDusts = screen.width / 60; // for RWD
+  const totalDusts = screen.height / 15; // for RWD
   console.log(totalDusts);
 
-  const dusts = Array(Math.floor(totalDusts)).fill(0).map(() => ({
-    x: Math.random() * resizeWidth,
-    y: Math.random() * resizeHeight,
-    xa: 2 * Math.random() - 1,
-    ya: 2 * Math.random() - 1,
-    max: 20
-  }))
-  console.log('dusts', dusts.length);
+  function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  const dusts = Array(Math.floor(totalDusts))
+    .fill(0)
+    .map(() => {
+      const z = getRandom(3, 7);
+      return {
+        x: Math.random() * resizeWidth,
+        y: Math.random() * resizeHeight,
+        inertia_x: 2 * Math.random() - 1,
+        inertia_y: 2 * Math.random() - 1,
+        radius: z,
+        max: 20,
+      };
+    });
+  console.log("dusts", dusts.length);
 
   const ctx = canvas.getContext("2d");
 
-  function update () {
-    dusts.forEach(dust => {
+  function update() {
+    dusts.forEach((dust) => {
       // 自動飄浮
-      dust.x += dust.xa;
-      dust.y += dust.ya;
+      dust.x += dust.inertia_x;
+      dust.y += dust.inertia_y;
 
       // 邊界碰撞
-      dust.xa *= dust.x > resizeWidth || dust.x < 0 ? -1 : 1;
-      dust.ya *= dust.y > resizeHeight || dust.y < 0 ? -1 : 1;
-
+      dust.inertia_x *= dust.x > resizeWidth || dust.x < 0 ? -1 : 1;
+      dust.inertia_y *= dust.y > resizeHeight || dust.y < 0 ? -1 : 1;
 
       if (mousePosition.x != null && mousePosition.y != null) {
         const diffX = dust.x - mousePosition.x;
         const diffY = dust.y - mousePosition.y;
-        const distanceMousePointToDust = Math.sqrt(diffX * diffX + diffY * diffY);
+        const distanceMousePointToDust = Math.sqrt(
+          diffX * diffX + diffY * diffY
+        );
         dust.distanceMousePointToDust = distanceMousePointToDust;
         if (distanceMousePointToDust < mousePosition.max) {
           // 引力場大小
           dust.x -= 0.03 * diffX; // 引力
           dust.y -= 0.03 * diffY; // 引力
         }
-        dust.rate = (mousePosition.max - distanceMousePointToDust) / mousePosition.max;
-      }
-      else {
+        dust.rate =
+          (mousePosition.max - distanceMousePointToDust) / mousePosition.max;
+      } else {
         dust.rate = 0.5;
       }
-    })
+    });
   }
-  function draw () {
+  function draw() {
     // clean all
     ctx.clearRect(0, 0, resizeWidth, resizeHeight);
 
-    dusts.forEach((dust, index) => {
+    // console.log("dusts", dusts);
+    dusts.forEach((dust) => {
       ctx.fillRect(dust.x - 0.5, dust.y - 0.5, 1, 1);
 
       ctx.beginPath();
@@ -103,17 +114,19 @@
       ctx.lineWidth = dust.rate / 2;
       ctx.strokeStyle = "rgba(0,0,0, " + dust.rate + 0.5 + ")";
       ctx.stroke();
+
       ctx.beginPath();
-      ctx.arc(dust.x, dust.y, 5, 0, 2 * Math.PI);
+      ctx.filter = `blur(${Math.abs(dust.radius - 5) * 1.5}px)`;
+      ctx.arc(dust.x, dust.y, dust.radius, 0, 2 * Math.PI);
       ctx.fillStyle = "#fff";
-      ctx.stroke()
-    })
+      ctx.stroke();
+    });
     requestAnimationFrame(() => {
-      setTimeout(update, 1)
-      draw()
+      setTimeout(update, 1);
+      draw();
     });
   }
   // setTimeout(update, 100);
-  update()
+  setTimeout(update, 1);
   draw();
-})()
+})();
