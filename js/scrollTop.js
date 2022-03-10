f8aeb6b67a2f6fea1337876abdcac3dc06071c9a
chris <dwatow@gmail.com>
2022-03-11 07:54:07 +0800

    Site updated: 2022-03-11 07:54:06

--- js/scrollTop.js
+++ js/scrollTop.js
@@ -1,15 +1,15 @@
 (() => {
-  console.log('scrolltop');
-  
-  document.querySelectorAll('.toc-link').forEach(link => {
-    link.addEventListener('click', function (e) {
+  // console.log('scrolltop');
+
+  document.querySelectorAll(".toc-link").forEach((link) => {
+    link.addEventListener("click", function (e) {
       e.preventDefault();
       const header = document.querySelector(decodeURIComponent(link.hash));
       scrollTo(header.offsetTop, 550);
-    })
-  })
+    });
+  });
 
-  function scrollTo (to, duration) {
+  function scrollTo(to, duration) {
     const element = document.documentElement;
     var start = element.scrollTop,
       change = to - start,
@@ -17,9 +17,10 @@
       increment = 20;
 
     var animateScroll = function () {
-      
       currentTime += increment;
-      var val = parseInt(Math.easeInOutQuad(currentTime, start, change, duration));
+      var val = parseInt(
+        Math.easeInOutQuad(currentTime, start, change, duration)
+      );
       console.log(val.toFixed(4));
       element.scrollTop = val;
       if (currentTime < duration) {
@@ -35,8 +36,8 @@
   //d = duration
   Math.easeInOutQuad = function (t, b, c, d) {
     t /= d / 2;
-    if (t < 1) return c / 2 * t * t + b;
+    if (t < 1) return (c / 2) * t * t + b;
     t--;
-    return -c / 2 * (t * (t - 2) - 1) + b;
+    return (-c / 2) * (t * (t - 2) - 1) + b;
   };
-})()
+})();
